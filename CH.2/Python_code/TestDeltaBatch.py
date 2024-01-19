d8afc8253bb5bf49718c99bf173b53e5e0c8187a
jkrau4 <jkrau0@gmail.com>
2024-01-19 14:25:53 +0900

    CH2

---
+++ CH.2/Python_code/TestDeltaBatch.py
@@ -0,0 +1,25 @@
+import numpy as np
+import Sigmoid
+import DeltaBatch
+
+X = np.array([[0, 0, 1],
+              [0, 1, 1],
+              [1, 0, 1],
+              [1, 1, 1]])
+
+D = np.array([[0],
+              [0],
+              [1],
+              [1]])
+
+W = 2*np.random.random((1, 3)) - 1
+
+for epoch in range(40000):
+  W = DeltaBatch(W, X, D)
+
+N = 4
+for k in range(N):
+      x = X[k,:].T
+      v = np.matmul(W, x)
+      y = Sigmoid(v)
+      print(y)
