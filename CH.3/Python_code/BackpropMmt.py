0c2fdf88391809f9742e6660d21b929adaa073b3
GUSEUL <157083869+GUSEUL@users.noreply.github.com>
2024-01-23 17:48:10 +0900

    Mmt

--- CH.3/Python_code/BackpropMmt.py
+++ CH.3/Python_code/BackpropMmt.py
@@ -2,25 +2,27 @@ import numpy as np
 from Sigmoid import Sigmoid
 
 def BackpropMmt(W1, W2, X, D):
-    alpha = 0.9
-    beta = 0.9
+    alpha = 0.9 # scalar
+    beta = 0.9 # scalar
 
-    mmt1 = np.zeros_like(W1)
-    mmt2 = np.zeros_like(W2)
+    mmt1 = np.zeros_like(W1) # (4, 3)
+    mmt2 = np.zeros_like(W2) # (1, 4)
 
     N = 4
     for k in range(N):
-        x = X[k, :].T
-        d = D[k]
+        
+        x = X[k, :].T # (3, 1)
+        d = D[k] # scalar
 
-        v1 = np.matmul(W1, x)
-        y1 = Sigmoid(v1)
-        v = np.matmul(W2, y1)
-        y = Sigmoid(v)
+        v1 = np.matmul(W1, x) # (1, 4)
+        y1 = Sigmoid(v1) # (1, 4)
+        v = np.matmul(W2, y1) # scalar
+        y = Sigmoid(v) # scalar
 
-        e = d - y
-        delta = y*(1-y)*e
+        e = d - y # scalar
+        delta = y * (1-y) * e # scalar
 
+<<<<<<< Updated upstream
         e1 = np.matmul((W2.T), delta)
         delta1 = y1*(1-y1)*e1
 
@@ -32,3 +34,17 @@ def BackpropMmt(W1, W2, X, D):
         mmt2 = dW2 + beta*mmt2
         W2 = W2 + mmt2
     return W1, W2
+=======
+        e1 = W2 * delta # (1, 4)
+        delta1 = y1 * (1 - y1) *e1 # (1, 4)
+
+        dW1 = (alpha * delta1).T * x.T #(4, 3)
+        mmt1 = dW1 + beta*mmt1 # (4, 3)
+        W1 = W1 + mmt1 # (4, 3)
+
+        dW2 = alpha * delta * y1.T # (1, 4)
+        mmt2 = dW2 + beta*mmt2 # (1, 4)
+        W2  = W2 + mmt2 # (1, 4)
+
+    return W1, W2   # (4, 3), (1, 4)
+>>>>>>> Stashed changes
--- CH.3/Python_code/TestBackpropMmt.py
+++ CH.3/Python_code/TestBackpropMmt.py
@@ -3,16 +3,23 @@ from BackpropMmt import BackpropMmt
 from Sigmoid import Sigmoid
 
 def TestBackpropMmt():
-  X = np.array([[0, 0, 1],
-               [0, 1, 1],
-               [1, 0, 1],
-               [1, 1, 1]])
+    X = np.array([[0, 0, 1],
+                  [0, 1, 1],
+                  [1, 0, 1],
+                  [1, 1, 1]]) #(4, 3)
     
-  D = np.array([[0],
-              [1],
-              [1],
-              [0]])
+    D = np.array([[0],
+                  [1],
+                  [1],
+                  [0]]) # (4, 1)
+    
+    W1 = 2*np.random.random((4, 3)) - 1
+    W2 = 2*np.random.random((1, 4)) - 1
+    
+    for epoch in range(10000):
+        W1, W2 = BackpropMmt(W1, W2, X, D)
         
+<<<<<<< Updated upstream
   W1 = 2*np.random.random((4, 3)) - 1
   W2 = 2*np.random.random((1, 4)) - 1
         
@@ -27,5 +34,15 @@ def TestBackpropMmt():
       v = np.matmul(W2, y1)
       y = Sigmoid(v)
       print(y)
+=======
+    N = 4
+    for k in range(N):
+        x  = X[k, :].T
+        v1 = np.matmul(W1, x)
+        y1 = Sigmoid(v1)
+        v  = np.matmul(W2, y1)
+        y  = Sigmoid(v)
+        print(y)
+>>>>>>> Stashed changes
 
 TestBackpropMmt()
